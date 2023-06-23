297767260fc0b2fb8cc78983142d1df9679fab38
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-06-24 01:04:49 +0530

    The Main Application class have add one line

--- src/main/java/com/books/TheLibraryApplication.java
+++ src/main/java/com/books/TheLibraryApplication.java
@@ -9,6 +9,7 @@ public class TheLibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TheLibraryApplication.class, args);
 		System.out.println("Program is running...");
+		System.out.println("Push in Git Hub");
 	}
 
 }
